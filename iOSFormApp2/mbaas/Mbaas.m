541a343b1f5bdaf39e5c29ec03553640faba5a3c
natsumo <ikeda.natsumo@gmail.com>
2018-09-14 19:06:17 +0900

    update project　Mbaas.m
    セミナー用に更新

--- iOSFormApp2/mbaas/Mbaas.m
+++ iOSFormApp2/mbaas/Mbaas.m
@@ -23,84 +23,84 @@
 /***** demo1：保存 *****/
 + (void) saveData:(NSString*)name email:(NSString*)email age:(NSNumber*)age prefecture:(NSString*)prefecture title:(NSString*)title contents:(NSString*)contents errorCallback:(void (^)( NSError *error)) errorCallback {
     // [demo1_1]保存先クラスの作成
-    NCMBObject *object = [NCMBObject objectWithClassName:@"Inquiry"];
+    
     // [demo1_2]データの設定
-    [object setObject:name forKey:@"name"];
-    [object setObject:email forKey:@"emailAddress"];
-    [object setObject:age forKey:@"age"];
-    [object setObject:prefecture forKey:@"prefecture"];
-    [object setObject:title forKey:@"title"];
-    [object setObject:contents forKey:@"contents"];
+    
+    
+    
+    
+    
+    
     // [demo1_3]データの保存処理の実行
-    [object saveInBackgroundWithBlock:^(NSError *error) {
-        //保存成功or失敗の処理
-        errorCallback(error);
-    }];
+    
+    
+    
+    
 }
 
 /***** demo2：全件検索 *****/
 + (void) getAllData: (void (^)(NSArray *objects)) successCallback errorCallback:(void (^)( NSError *error)) errorCallback {
     // [demo2_1]保存先クラスのクエリを生成
-    NCMBQuery *query = [NCMBQuery queryWithClassName:@"Inquiry"];
+    
     // [demo2_2]検索条件の設定（保存日時の降順）
-    [query orderByDescending:@"createDate"];
+    
     // [demo2_3]全件検索処理の実行
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (error) {
-            // 検索失敗時の処理
-            errorCallback(error);
-        } else {
-             // 検索成功時の処理
-            successCallback(objects);
-        }
-    }];
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 /***** demo3-1：条件検索 *****/
 + (void) getSearchData:(NSString*)q searchBy:(SearchByEnum)searchBy successCallback:(void (^)(NSArray *objects)) successCallback errorCallback:(void (^)( NSError *error)) errorCallback {
     // [demo3-1_1]保存先クラスのクエリを生成
-    NCMBQuery *query = [NCMBQuery queryWithClassName:@"Inquiry"];
+    
     // [demo3-1_2]検索条件の設定（保存日時の降順）
-    [query orderByDescending:@"createDate"];
+    
     
     if (SearchByEmail == searchBy) {
         // [demo3-1_3]検索条件（メールアドレス）の設定
-        [query whereKey:@"emailAddress" equalTo:q];
+        
     } else {
         // [demo3-1_4]検索条件（都道府県）の設定
-        [query whereKey:@"prefecture" equalTo:q];
+        
     }
     // [demo3-1_5]全件検索処理の実行
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (error) {
-            // 検索失敗時の処理
-            errorCallback(error);
-        } else {
-            // 検索成功時の処理
-            successCallback(objects);
-        }
-    }];
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 /***** demo3-2：条件検索（範囲指定） *****/
 + (void) getRangeSearchData:(NSNumber*)ageGreaterThan ageLessThan:(NSNumber*)ageLessThan successCallback:(void (^)(NSArray *objects)) successCallback errorCallback:(void (^)( NSError *error)) errorCallback {
     // [demo3-2_1]保存先クラスのクエリを生成
-    NCMBQuery *query = [NCMBQuery queryWithClassName:@"Inquiry"];
+    
     // [demo3-2_2]検索条件の設定（保存日時の降順）
-    [query orderByDescending:@"createDate"];
+    
     // [demo3-2_3]検索条件（年齢）の設定（○○歳以上）
-    [query whereKey:@"age" greaterThanOrEqualTo:ageGreaterThan];
+    
     // [demo3-2_4]検索条件（年齢）の設定（○○歳未満）
-    [query whereKey:@"age" lessThan:ageLessThan];
+    
     // [demo3-2_5]全件検索処理の実行
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (error) {
-            // 検索失敗時の処理
-            errorCallback(error);
-        } else {
-            // 検索成功時の処理
-            successCallback(objects);
-        }
-    }];
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 @end
